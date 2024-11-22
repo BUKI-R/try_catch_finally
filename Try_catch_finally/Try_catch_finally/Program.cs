ed84668d6eaf25f31b28a5d1fb47a79258e18b46
BUKI <buyunov2005@gmail.com>
2024-11-22 14:36:11 +0900

    понятная версия

---
+++ Try_catch_finally/Try_catch_finally/Program.cs
@@ -0,0 +1,16 @@
+﻿
+try
+{
+    Console.WriteLine("Деление не на ноль");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = a / b;
+}
+catch 
+{
+    Console.WriteLine("непраильно");
+}
+finally
+{
+    Console.WriteLine($"правельно");
+}
